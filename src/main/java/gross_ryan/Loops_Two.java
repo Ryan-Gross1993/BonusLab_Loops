28954e38174f7e10b0c93004e2666b2bac679187
Ryan Gross <rgross@udel.edu>
2017-01-22 22:09:16 -0500

    Added tableSquares and made it scalable.

--- src/main/java/gross_ryan/Loops_Two.java
+++ src/main/java/gross_ryan/Loops_Two.java
@@ -1,5 +1,7 @@
 package gross_ryan;
 
+import java.util.Random;
+
 /**
  * Created by ryangross on 1/16/17.
  */
@@ -13,8 +15,98 @@ public class Loops_Two {
         return answer;
     }
 
+    public String oddNumbers() {
+        String answer = "oddNumbers()\n" +
+                "*** Output ***\n";
+        for(int i = 1; i < 20; i = i + 2) {
+            answer += i + "\n";
+        }
+       return answer;
+    }
+
+    public String squares() {
+        String answer = "squares()\n" +
+                "*** Output ***\n";
+        int square = 0;
+        for(int i = 1; square < 100; i++) {
+            square = (int) Math.pow(i,2);
+            answer += square + "\n";
+        }
+        return answer;
+    }
+
+    public String random4() {
+        String answer = "random4()\n" +
+                "*** Output ***\n";
+        Random aRandom = new Random();
+        for(int i = 0; i < 10; i++) {
+            answer += aRandom.nextInt(10);
+        }
+        return answer;
+    }
+
+    public String even(int aNumber) {
+        String answer ="even" + aNumber + "()" + "\n" + "*** Output ***\n";
+        for (int i = 1; i < aNumber; i++) {
+            if (i % 2 == 0) {
+                answer += i + "\n";
+            }
+        }
+        return answer;
+    }
+
+    public String tables(int x) {
+        String answer = "";
+        for(int i = 1; i <= x; i++) {
+            String row = "";
+            for(int j = 1; j <= x; j++) {
+                row += " | " + (i * j);
+            }
+            answer += row + " |\n";
+        }
+        return answer;
+    }
+
+    public String powers(int n) {
+        String answer = "*** Output ***\n";
+        for(int i = 1; i <= n; i++) {
+            answer += (int) Math.pow(2, i) + "\n";
+        }
+        return answer;
+    }
+
+    public String addSpaces(int numOfSpaces) {
+        String spaces = "";
+
+        for(int i = 0; i < numOfSpaces; i++) {
+            spaces += " ";
+        }
+        return spaces;
+    }
+
+
+    public String tableSquares(int n ) {
+        String finalAnswer = "";
+        for (int i = 1; i <= n; i++) {
+            String indivLine = "|";
+            for (int j = 1; j <= n; j++) {
+                int maxDigits = String.valueOf(n * n).length();
+                int current = String.valueOf(i * j).length();
+                if(current != maxDigits) {
+                    indivLine += " " + addSpaces(maxDigits - current) + (i*j) + " |";
+                } else {
+                    indivLine += " " + (i*j) + " |";
+                }
+            }
+            finalAnswer += indivLine + "\n";
+        }
+        return finalAnswer;
+    }
     public static void main(String[] args) {
         Loops_Two main = new Loops_Two();
-        main.oneToTen();
+    //    main.oneToTen();
+      //  main.oddNumbers();
+       // System.out.println(main.tables(3));
+        System.out.println(main.tableSquares(10));
     }
 }
--- src/test/java/gross_ryan/Loops_TwoTest.java
+++ src/test/java/gross_ryan/Loops_TwoTest.java
@@ -32,4 +32,63 @@ public class Loops_TwoTest {
                 "10\n";
         Assert.assertEquals(expected, test.oneToTen());
     }
+
+    @Test
+    public void oddNumbersTest() {
+        String expected = "oddNumbers()\n" +
+                "*** Output ***\n" +
+                "1\n" +
+                "3\n" +
+                "5\n" +
+                "7\n" +
+                "9\n" +
+                "11\n" +
+                "13\n" +
+                "15\n" +
+                "17\n" +
+                "19\n";
+        Assert.assertEquals(expected, test.oddNumbers());
+    }
+
+    @Test
+    public void squaresTest() {
+        String expected = "squares()\n" +
+                "*** Output ***\n" +
+                "1\n" +
+                "4\n" +
+                "9\n" +
+                "16\n" +
+                "25\n" +
+                "36\n" +
+                "49\n" +
+                "64\n" +
+                "81\n" +
+                "100\n";
+        Assert.assertEquals(expected, test.squares());
+    }
+
+    @Test
+    public void evenTest() {
+        String expected = "even10()\n" +
+                "*** Output ***\n" +
+                "2\n" +
+                "4\n" +
+                "6\n" +
+                "8\n";
+        Assert.assertEquals(expected, test.even(10));
+    }
+
+    @Test
+    public void powersTest() {
+        String expected = "*** Output ***\n" +
+                "2\n" +
+                "4\n" +
+                "8\n" +
+                "16\n" +
+                "32\n" +
+                "64\n" +
+                "128\n" +
+                "256\n";
+        Assert.assertEquals(expected, test.powers(8));
+    }
 }
